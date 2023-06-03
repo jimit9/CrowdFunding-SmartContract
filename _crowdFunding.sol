08868da9c643e5e34a790325dd6f362b8471e58d
jimit <jimit@Jimits-MacBook-Air.local>
2023-06-03 11:25:30 +0530

    initial commit

---
+++ _crowdFunding.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.0 < 0.9.0;
+contract crowdFunding{
+    mapping(address=>uint) public contributors;
+    address public manager;
+    uint public minContribution;
+    uint public deadline;
+    uint public target;
+    uint public raisedAmount;
+    uint public numOfContributors;
+    constructor(uint _minContribution,uint _deadline,uint _target){
+        minContribution=_minContribution;
+        deadline=_deadline;
+        target=_target;
+        manager=msg.sender;
+        
+    }
+}
